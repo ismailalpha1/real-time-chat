60b56fae0adc7bae5745be6ddf02e233f7b34df0
ismailalpha1 <ijalali626@gmail.com>
2026-04-14 20:19:27 +0200

    Add a seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use App\Models\User;
+use App\Models\Message;
+use App\Models\Conversation;
+use App\Models\Group;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,6 +24,44 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true
         ]);
+
+        User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        User::factory(10)->create();
+
+        for ($i=0; $i < 5; $i++) {
+            $group = Group::factory()->create([
+                'owner_id'=>1,
+            ]);
+
+            $users = User::inRandomOrder()->limit(rand(2,5))->pluck('id');
+            $group->users()->attach(array_unique([1, ...$users]));
+        }
+
+        Message::factory(100)->create();
+        $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
+
+        $converations = $messages->groupBy(function($messages){
+            return collect([
+                $message->sender_id, $message->receiver_id
+            ])->sort()->implode('_');
+        })->map(function ($groupedMessages) {
+            return [
+                'user_id1'=> $groupedMessages->first()->sender_id,
+                'user_id2'=> $groupedMessages->first()->receiver_id,
+                'last_message_id'=> $groupedMessages->last()->id,
+                'created_at' => new Carbon(),
+                'updated_at' => new Carbon(),
+            ];
+        })->values();
+
+        Conversation::insertOrIgnore($converations->toArray());
     }
 }
